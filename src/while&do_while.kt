cbf35d6b8c912b79882a8976b6834bff7d3e7d8f
Naheda Pathan <nahedapathan5@gmail.com>
2025-10-17 12:22:40 -0400

    Added the example of while and do while loop

---
+++ src/while&do_while.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var i=1
+  /*  while (i<=5){
+        print("number : $i\n")
+        i++
+    }*/
+
+    do{
+        print("number :$i\n")
+        i++
+
+    }while(i<=5)
+
+
+}
